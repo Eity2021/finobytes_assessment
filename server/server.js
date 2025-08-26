9fea4abab1c23a404676de086eee0f7555cb1966
Eity2021 <Eity2021akter@gmail.com>
2025-08-26 22:36:37 +0600

    add vercel.json

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import bcrypt from "bcryptjs";
 import "dotenv/config";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({ origin: true, credentials: true }));
